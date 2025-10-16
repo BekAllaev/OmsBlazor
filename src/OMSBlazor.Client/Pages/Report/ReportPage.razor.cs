c4b7ddbac6021cca630621e561e923d3a425fc72
Bekzod Allaev <Allaev_@outlook.com>
2025-10-16 17:00:42 +0200

    Clear folders from not used projects

---
+++ src/OMSBlazor.Client/Pages/Report/ReportPage.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Reporting.Pages.Services;
+
+namespace OMSBlazor.Client.Pages.Report
+{
+    public partial class ReportPage
+    {
+        private readonly NavigationManager navigationManager;
+        private readonly IJsonDataSourceUpdater jsonDataSourceUpdater;
+        private readonly HubConnection hub;
+
+        private bool _subscribed;
+
+        public ReportPage(NavigationManager navigationManager, IJsonDataSourceUpdater jsonDataSourceUpdater, HubConnection hub)
+        {
+            this.navigationManager = navigationManager;
+            this.jsonDataSourceUpdater = jsonDataSourceUpdater;
+            this.hub = hub;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+
+            if (!_subscribed)
+            {
+                //hub.On<>("ReceiveAllStats", async data =>
+                //{
+                //    await jsonDataSourceUpdater.UpdateDataSourceAsync(data);
+                //    navigationManager.NavigateTo(navigationManager.Uri, true);
+                //});
+                _subscribed = true;
+            }
+
+            if (hub.State == HubConnectionState.Disconnected)
+                await hub.StartAsync();
+
+        }
+    }
+}
